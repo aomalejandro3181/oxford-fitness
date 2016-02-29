677b4ad3f38725c3f48b684ae473966126ffaefa
Alejandro Olivar <aolivar@teravisiontech.com>
2016-02-29 19:04:06 -0430

    install framework and add materialize

---
+++ application/views/login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+    <head>
+    <title>Login</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="" />
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/materialize.min.css"); ?>" />    
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/index.css"); ?>" />
+
+    </head>
+    <body>
+        <section class="container body">
+            <figure class="center"><img src="http://placehold.it/200x100"></figure>
+            <div class="row">
+                <div class="card-panel center col s12 m6 l6 offset-l3">
+                    hola
+                </div>
+            </div>
+        </section>
+    </body>
+</html>
